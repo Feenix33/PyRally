3cfd77a8f3ee4482497a8dfdc297be7c59eb8669
Eberhardt <c.eberhardt.dfw@gmail.com>
2019-04-18 09:23:20 -0500

    Added opt processing

--- getTree.py
+++ getTree.py
@@ -7,15 +7,13 @@ Pull everything from the input point
 everything = PGM > PRJ > FEA > TF > US
 
 To Do:
-    Defect as single entry
-    Defects from one of the everything if available
-    Only pull specific type from input flag
-    Only pull down to specific type from input flag
-    Change writing to file for speed
-    Multiple input
 """
 
+# Globals
 errout = sys.stderr.write
+gblProcUserStory = True # global to process at the user story level
+gblProcTeamFeature = True # global to process at the user story level
+glbOutName = ""
 
 gFields = [ #"FormattedID",
         "Name", 
@@ -28,6 +26,8 @@ gFields = [ #"FormattedID",
         "UnEstimatedLeafStoryCount",
         "LeafStoryPlanEstimateTotal",
         "AcceptedLeafStoryPlanEstimateTotal",
+        #"State",
+        #"TTSDefectState",
         ]
 
 def returnAttrib(item, attr, default=""):
@@ -45,6 +45,22 @@ def printHelp():
 def main(args):
     options = [opt for opt in args if opt.startswith('-')]
     args    = [arg for arg in args if arg not in options]
+    global gblProcUserStory
+    global gblProcTeamFeature
+    global glbOutName
+
+    for opt in options:
+        if opt == '-us': 
+            gblProcUserStory = False
+        elif opt == '-tf':
+            gblProcTeamFeature = False
+        elif opt == '-h':
+            printHelp()
+            sys.exit(3)
+        elif opt[:2] == '-o':
+            if len(opt) > 2:
+                glbOutName = opt[2:]
+
     if len(args) < 1:
         errout('ERROR: Wrong number of arguments\n')
         printHelp()
@@ -57,7 +73,10 @@ def main(args):
     rally = Rally(server, apikey=apikey, workspace=workspace, project=project)
 
     bPrintStatus = True
-    fileName = 'out.csv'
+    if glbOutName == "":
+        fileName = args[0] + '.csv'
+    else:
+        fileName = glbOutName + '.csv'
 
     with open (fileName, 'w', newline='', encoding="utf-8") as csvfile:
         #write header
@@ -102,6 +121,8 @@ def processQueryTokens(rally, bPrintStatus, outfile, token):
                 processTF(outfile, "", "", "", item)
             elif queryToken[:2] == "US":
                 processUS(outfile, "", "", "", "", item)
+            elif queryToken[:2] == "DE":
+                processUS(outfile, "", "", "", "", item)
 
 
 def processPGM(outfile, pgm):
@@ -125,15 +146,16 @@ def processFEA(outfile, pgmID, prjID, fea):
     outrow = ["FEA", pgmID, prjID, fea.FormattedID, "", "" ] + \
             [returnAttrib(fea, field, default="") for field in gFields]
     outfile.writerow(outrow)
-    if hasattr(fea, "Children"):
+    if gblProcTeamFeature and hasattr(fea, "Children"):
         for tf in fea.Children:
             processTF(outfile, pgmID, prjID, fea.FormattedID, tf)
+
 
 def processTF(outfile, pgmID, prjID, feaID, tf):
     outrow = ["TF", pgmID, prjID, feaID, tf.FormattedID, "" ] + \
         [returnAttrib(tf, field, default="") for field in gFields]
     outfile.writerow(outrow)
-    if hasattr(tf, "UserStories"):
+    if gblProcUserStory and hasattr(tf, "UserStories"):
         for us in tf.UserStories:
             processUS(outfile, pgmID, prjID, feaID, tf.FormattedID, us)
 
@@ -141,55 +163,18 @@ def processUS(outfile, pgmID, prjID, feaID, tfID, us):
     outrow = ["US", pgmID, prjID, feaID, tfID,  us.FormattedID  ] + \
         [returnAttrib(us, field, default="") for field in gFields]
     outfile.writerow(outrow)
+    #skip defects for now
+    #if hasattr(us, "Defects"):
+    #    for defect in us.Defects:
+    #        processDE(outfile, pgmID, prjID, feaID, us.FormattedID, defect)
+
+
+def processDE(outfile, pgmID, prjID, feaID, tfID, defect):
+    outrow = ["DE", pgmID, prjID, feaID, tfID,  defect.FormattedID  ] + \
+        [returnAttrib(defect, field, default="") for field in gFields]
+    outfile.writerow(outrow)
 
 
 if __name__ == '__main__':
     main(sys.argv[1:])
 
-
-
-
-"""
-    # form the query
-    queryToken = args[0].upper()  #only search first one for now
-    if queryToken[0] == "D":
-        entityName = 'Defect'
-    elif queryToken[0] == "U":
-        entityName = 'HierarchicalRequirement'
-    else:
-        entityName = 'PortfolioItem'
-
-    queryString = 'FormattedID = "' + queryToken + '"'
-    fileName = 'out.csv'
-
-    if bPrintStatus: print ("Query = ", queryString, fileName)
-
-    if bPrintStatus: print ('Logging in...')
-    rally = Rally(server, apikey=apikey, workspace=workspace, project=project)
-
-    response = rally.get(entityName, fetch=True, projectScopeDown=True, query=queryString)
-
-    if response.resultCount == 0:
-        errout('No item found for %s %s\n' % (entityName, arg))
-        print ("Nothing")
-    else:
-        if bPrintStatus: print ("Printing to '%s'" % fileName)
-        with open (fileName, 'w', newline='', encoding="utf-8") as csvfile:
-            #write header
-            outfile = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            outrow = ["Type", "PGM", "PRJ", "FEA.ID", "TF.ID", "US.ID"] + \
-                        [field for field in gFields]
-            outfile.writerow(outrow)
-
-            for item in response:
-                if queryToken[:2] == "PG":
-                    processPGM(outfile, item)
-                elif queryToken[:2] == "PR":
-                    processPRJ(outfile, "", item)
-                elif queryToken[:2] == "FE":
-                    processFEA(outfile, "", "", item)
-                elif queryToken[:2] == "TF":
-                    processTF(outfile, "", "", "", item)
-                elif queryToken[:2] == "US":
-                    processUS(outfile, "", "", "", "", item)
-"""
